261ecc866ef041f053504f0d055514667019e23d
Anwesh43 <anweshthecool0@gmail.com>
2021-02-17 18:21:45 +0530

    adding fixes

--- multisidebarview/src/main/java/com/example/multisidebarview/MultiSideBarView.kt
+++ multisidebarview/src/main/java/com/example/multisidebarview/MultiSideBarView.kt
@@ -26,7 +26,7 @@ val scGap : Float = 0.02f / parts
 
 
 fun Int.inverse() : Float = 1f / this
-fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this * n.inverse())
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
 
