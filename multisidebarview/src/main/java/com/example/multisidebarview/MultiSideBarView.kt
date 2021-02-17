09b8fbf14ea59ecb9e7bcbee6bd825162ddb79d9
Anwesh43 <anweshthecool0@gmail.com>
2021-02-17 16:36:31 +0530

    adding constants and libraries

--- multisidebarview/src/main/java/com/example/multisidebarview/MultiSideBarView.kt
+++ multisidebarview/src/main/java/com/example/multisidebarview/MultiSideBarView.kt
@@ -1,2 +1,27 @@
 package com.example.multisidebarview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#2196F3",
+    "#8BC34A",
+    "#E91E63",
+    "#03A9F4"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val delay : Long = 20
+val sizeFactor : Float = 8.9f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+
+
