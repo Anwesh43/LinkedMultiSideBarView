b83a7844d47fad9f5fe49f45422f588589fb9741
Anwesh43 <anweshthecool0@gmail.com>
2021-02-17 17:11:03 +0530

    adding MultiSideBarView class

--- multisidebarview/src/main/java/com/example/multisidebarview/MultiSideBarView.kt
+++ multisidebarview/src/main/java/com/example/multisidebarview/MultiSideBarView.kt
@@ -57,3 +57,19 @@ fun Canvas.drawMSBNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawMultiSideBar(scale, w, h, paint)
 }
+
+class MultiSideBarView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
