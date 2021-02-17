3fb8e2293ae4d8d633f2010d5a3963fc211a6a9b
Anwesh43 <anweshthecool0@gmail.com>
2021-02-17 17:35:24 +0530

    adding Renderer class

--- multisidebarview/src/main/java/com/example/multisidebarview/MultiSideBarView.kt
+++ multisidebarview/src/main/java/com/example/multisidebarview/MultiSideBarView.kt
@@ -184,4 +184,27 @@ class MultiSideBarView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : MultiSideBarView) {
+
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val animator : Animator = Animator(view)
+        private val msb : MultiSideBar = MultiSideBar(0)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            msb.draw(canvas, paint)
+            animator.animate {
+                msb.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            msb.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
